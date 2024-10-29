7f079aa3a22519f180863b506360af1c1cd9ee19
lossany <lossany08@gmail.com>
2024-10-30 00:44:17 +0700

    sementara

--- components/layout/main/main.tsx
+++ components/layout/main/main.tsx
@@ -1,8 +1,13 @@
 import { FC } from "react";
+import clsx from "clsx";
 import styles from "./index.module.scss"
 
 const Main: FC = () => {
     return (
-        <div></div>
+        <div className={clsx(styles)}>
+
+        </div>
     )
 }
+
+export default Main
